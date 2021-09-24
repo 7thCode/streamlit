5bb940df31c7b11541dea2eaaaadb1be966c7148
MIKIO ODA <oda.mikio@gmail.com>
2021-09-25 08:54:52 +0900

    ...

--- st_altair.py
+++ st_altair.py
@@ -3,14 +3,31 @@
 import streamlit as st
 import altair as alt
 from vega_datasets import data
+from matplotlib import pyplot as plt
+import time
+
 import webbrowser
 
 def main():
 
-    url = 'https://altair-viz.github.io/'
+    labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+    men_means = [20, 35, 30, 35, 27]
+    women_means = [25, 32, 34, 20, 25]
+    men_std = [2, 3, 4, 1, 2]
+    women_std = [3, 5, 2, 3, 3]
+    width = 0.35       # the width of the bars: can also be len(x) sequence
 
-    if st.button('altair'):
-        webbrowser.open_new_tab(url)
+    fig, ax = plt.subplots()
+
+    ax.bar(labels, men_means, width, yerr=men_std, label='Men')
+    ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means, label='Women')
+
+    ax.set_ylabel('Scores')
+    ax.set_title('Scores by group and gender')
+    ax.legend()
+
+    st.pyplot(fig)
+
 
     source = data.cars()
 
@@ -25,6 +42,19 @@ def main():
     ).interactive()
 
     st.write(fig)
+
+    status_text = st.empty()
+    # プログレスバー
+    progress_bar = st.progress(0)
+
+    for i in range(100):
+        status_text.text(f'Progress: {i}%')
+        # for ループ内でプログレスバーの状態を更新する
+        progress_bar.progress(i + 1)
+        time.sleep(0.1)
+
+    status_text.text('Done!')
+    st.balloons()
 
 if __name__ == '__main__':
     main()
